39e456ebb4123a2c5663f5b16ea96629b4cd83f6
chickky2408 <hannypimh@gmail.com>
2025-04-25 21:41:07 +0700

    edit

--- app/api/doctors/[id]/route.ts
+++ app/api/doctors/[id]/route.ts
@@ -1,13 +1,43 @@
+// import { PrismaClient } from '@prisma/client'
+// import { NextRequest, NextResponse } from 'next/server'
+
+// const prisma = new PrismaClient()
+
+// export async function GET(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const doctorId = params.id
+
+//   try {
+//     const doctor = await prisma.doctor.findUnique({
+//       where: { id: doctorId },
+//     })
+
+//     if (!doctor) {
+//       return NextResponse.json({ message: 'Doctor not found' }, { status: 404 })
+//     }
+
+//     return NextResponse.json(doctor)
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json({ message: 'Server error' }, { status: 500 })
+//   }
+// }
+
+
+
+
 import { PrismaClient } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
-  const doctorId = params.id
+  const doctorId = context.params.id
 
   try {
     const doctor = await prisma.doctor.findUnique({
@@ -24,7 +54,3 @@ export async function GET(
     return NextResponse.json({ message: 'Server error' }, { status: 500 })
   }
 }
-
-
-
-
